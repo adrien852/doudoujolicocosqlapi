d58165055beb7a9edfcc8a83c799389552b8f286
Adrien <adrien@traveldepartment.ie>
2023-11-22 16:47:26 +0000

    port env

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -28,7 +28,8 @@ myDataSource.initialize()
   console.log("Data Source has been initialized!");
 })
 
-app.listen(8080, () => {
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
   console.log('Server running on port 8080');
 });
 
